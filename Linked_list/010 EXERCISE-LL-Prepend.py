8232a2bbfdc2d882518308a22a5ba8e0dcabd232
annah11 <hana.mesfin@astu.edu.et>
2024-11-11 17:41:20 +0300

    prepend

--- Linked_list/010 EXERCISE-LL-Prepend.py
+++ Linked_list/010 EXERCISE-LL-Prepend.py
@@ -43,7 +43,19 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp
-
+    def prepend(self,value):
+       new_node = Node(value)
+       if self.length == 0:
+           self.head = new_node
+           self.tail = new_node
+           return new_node
+       else:
+           new_node.next = self.head
+           self.head = new_node
+           return new_node
+       self.length += 1
+       return True
+        
     ## WRITE PREPEND METHOD HERE ##
     #                             #
     #                             #
