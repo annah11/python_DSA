2503c4c5ec38dd2b94b1df12a4c68b2f14b8e110
annah11 <hana.mesfin@astu.edu.et>
2024-11-11 17:54:05 +0300

    pop_first

--- Linked_list/tempCodeRunnerFile.py
+++ Linked_list/tempCodeRunnerFile.py
@@ -1,3 +1,29 @@
-print(my_linked_list.pop().value)
-# # (0) Items - Returns None
-# print(my_linked_list.pop())
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
